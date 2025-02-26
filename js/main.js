24c3f39afd5741aeda14e5451c59e95856e69fcd
marcoberrocal <2405217@edu.karelia.fi>
2025-02-26 19:49:43 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ $(function() {
 	'use strict';
 	
 $(".loader").delay(700).fadeOut("slow");
-  $("#untree_co--overlayer").delay(700).fadeOut("slow");	
+  $("#untree_co--overlayer").delay(200).fadeOut("fast");	
 
 	var jarallaxPlugin = function() {
 		objectFitImages();
